679e73b0124ac9e5fa0ea9d7acb9f7bd007fa804
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-10-30 15:16:14 +0900

    Error: listen EADDRINUSE: address already in use 4000;

--- db.js
+++ db.js
@@ -1,50 +1,18 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, 
   {
-    id: 123,
-    title: "Video Awesome",
-    description: "This is one of the most popular video between our subscribers",
-    views: 1000000,
-    videoFile: "https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4",
-    creator: {
-      id: 123123,
-      name: "Aiden",
-      email: "aiden@nomadcoder.com"
-    }
-  },
-  {
-    id: 234,
-    title: "Video Cool",
-    description: "This is one of the most popular video between our subscribers",
-    views: 1000000,
-    videoFile: "https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4",
-    creator: {
-      id: 234234,
-      name: "Aiden",
-      email: "aiden@nomadcoder.com"
-    }
-  },
-  {
-    id: 345,
-    title: "Video Nice",
-    description: "This is one of the most popular video between our subscribers",
-    views: 1000000,
-    videoFile: "https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4",
-    creator: {
-      id: 345345,
-      name: "Aiden",
-      email: "aiden@nomadcoder.com"
-    }
-  },
-  {
-    id: 456,
-    title: "Video Brilliant",
-    description: "This is one of the most popular video between our subscribers",
-    views: 1000000,
-    videoFile: "https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4",
-    creator: {
-      id: 456456,
-      name: "Aiden",
-      email: "aiden@nomadcoder.com"
-    }
-  },
-]
+    useNewUrlParser: true,
+    useFindAndModify: false 
+  }
+);
+
+const db = mongoose.connection;
+
+const checkOpen = () => console.log("✅ Connected to DB");
+const checkError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", checkOpen);
+db.on("error", checkError);
--- init.js
+++ init.js
@@ -1,8 +1,11 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-const handleListening = () =>
+const checkListening = () =>
   console.log(`✅Listening on: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+app.listen(PORT, checkListening);
