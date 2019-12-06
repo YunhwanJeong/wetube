641074def69dc391e22eb05eeea2b41781963ffe
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-12-06 16:31:21 +0900

    Fix github login

--- src/passport.js
+++ src/passport.js
@@ -17,7 +17,9 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: process.env.PRODUCTION
+        ? `https://floating-reaches-06734.herokuapp.com${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
   )
