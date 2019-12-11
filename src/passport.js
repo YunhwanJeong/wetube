80b42eeef1ba6cb21e6abca9c1f8cd34d26f06a0
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-12-11 18:16:40 +0900

    Fix small bug in facebook login callback URL

--- src/passport.js
+++ src/passport.js
@@ -29,7 +29,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      callbackURL: `https://floating-reaches-06734.herokuapp.com/${routes.facebookCallback}`,
+      callbackURL: `https://floating-reaches-06734.herokuapp.com${routes.facebookCallback}`,
       profileFields: ["id", "displayName", "photos", "email"]
     },
     facebookLoginCallback
